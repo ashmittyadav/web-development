13e848567c42ee8501287f534d816279053a1ccb
ashister <ashmityadavat92@gmail.com>
2026-07-01 17:06:11 +0530

    Added React implementation

---
+++ React/01-React/src/Clg.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const College = () => {
+  const clg = 'RKGIT'
+  const course = 'B.Tech IT'
+  const lang = 'java'
+  return (
+    <div>
+        <p>College : {clg}</p>
+        <p>Course : {course}</p>
+        <p>Favorite Language : {lang}</p>
+    </div>
+  )
+}
+
+export default College
