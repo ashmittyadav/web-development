4afb8492b48fb147a732cf5c034ce739a889357f
ashister <ashmityadavat92@gmail.com>
2026-07-03 20:44:11 +0530

    Implemented css

---
+++ JavaScript/cssJS/index.js
@@ -0,0 +1,13 @@
+
+let Element = document.getElementById('fpara');
+Element.style.backgroundColor = 'blue';
+
+let paraElement = document.getElementById('spara');
+paraElement.style.cssText = "background-color:pink; color:grey;padding:4rem";
+
+let firstElement = document.querySelector('#fdiv');
+firstElement.setAttribute('class','divClass');
+firstElement.setAttribute('class','secondDivClass');
+
+
+
