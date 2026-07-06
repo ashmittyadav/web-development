ddfe6632afe10b62ecd483d873808536e6d27fab
ashister <ashmityadavat92@gmail.com>
2026-07-06 22:16:28 +0530

    Reorganized BrowserEvents

--- JavaScript/BrowserEvents/index.js
+++ JavaScript/BrowserEvents/index.js
@@ -1,17 +1,45 @@
 
 
-    // function changeText(event) {
-    //     console.log(event);
-    //     let fpara = document.getElementById('fpara');
-    //     fpara.textContent = "ashmit"; 
-    // }
-    // let fpara = document.getElementById('fpara');
-    // fpara.addEventListener('click', changeText);
-    // // fpara.removeEventListener('click', changeText);
+//     function changeText(event) {
+//         console.log(event);
+//         let fpara = document.getElementById('fpara');
+//         fpara.textContent = "ashmit"; 
+//     }
+//     let fpara = document.getElementById('fpara');
+//     fpara.addEventListener('click', changeText);
+//     // fpara.removeEventListener('click', changeText);
 
-let anchorElement = document.getElementById('fanchor');
 
-anchorElement.addEventListener('click', function(event) {
-    event.preventDefault();
-    anchorElement.textContent = "done sir"
-});
+
+
+
+//default action
+// let anchorElement = document.getElementById('fanchor');
+// anchorElement.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     anchorElement.textContent = "done sir"
+// });
+
+
+
+//avoiding too many listeners
+// let paras = document.querySelectorAll('p');
+
+function alertPara(event) {
+    if(event.target.nodeName == 'SPAN'){
+
+        alert("you have clicked on para : "+ event.target.textContent);
+    }
+}
+
+// for(let i = 0; i < paras.length ; i++) {
+//     let para = paras[i];
+//     para.addEventListener('click',alertPara)
+//         // alert("you have clicked on para : "+(i+1));
+// }
+
+
+
+//using div
+let mydiv = document.getElementById('wrap');
+    document.addEventListener('click',alertPara)
